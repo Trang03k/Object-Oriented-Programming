8e6ce4075f5885cd6834e8b7a78984ea5cb85d5f
Trang03k <nguty406@mymail.unisa.edu.au>
2023-04-23 23:02:05 +0930

    Delete temperature_converter.py

--- workshop8/temperature_converter.py
+++
@@ -1,8 +0,0 @@
-
-
-def f_to_c(f):
-    '''Convert argument temperature from fahrenheit to celsius.'''
-    if (isinstance(f, int) or isinstance(f, float)):
-        return (f-32)*(5/9)
-    else:
-        raise TypeError("Only int or float is allowed.")
